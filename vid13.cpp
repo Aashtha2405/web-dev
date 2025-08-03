ea6fd13c5b214ecc2428f221f1fa5b88ef4eb117
Aashtha Kumari <202515293+Aashtha2405@users.noreply.github.com>
2025-08-03 19:24:37 +0530

    Add files via upload

---
+++ vid13.cpp
@@ -0,0 +1,15 @@
+//Arrays is a collection of similar type of data items stored in contiguous memory locations
+#include<iostream>
+using namespace std;
+int main(){
+    int marks[4] = {76,42,24,22};
+    cout<<marks[0]<<endl;
+    cout<<marks[1]<<endl;
+    cout<<marks[2]<<endl;
+    cout<<marks[3]<<endl;
+    return 0;
+}
+
+
+
+//pointers are data types which hold the address of other data types
